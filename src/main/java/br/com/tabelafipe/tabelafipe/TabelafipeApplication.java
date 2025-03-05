1b5bd33b65360ddf59439f060ba787abb01a6640
Wev kleyton <kleyton.cpd@gmnail.com>
2025-03-05 15:47:53 -0300

    Inicialização do projeto de Tabela Fipe

---
+++ src/main/java/br/com/tabelafipe/tabelafipe/TabelafipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tabelafipe.tabelafipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelafipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelafipeApplication.class, args);
+	}
+
+}
